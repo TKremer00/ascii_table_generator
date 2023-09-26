7d2dcefc7b382577ba1f52ecc3afc204989a02e6
Tim Kremer <tim7voetbal@live.nl>
2023-09-26 18:57:22 +0200

    removed commented code

--- ascii_table_frontend/src/lib.rs
+++ ascii_table_frontend/src/lib.rs
@@ -3,16 +3,6 @@ use js_sys::Array;
 use wasm_bindgen::prelude::wasm_bindgen;
 use wasm_bindgen::prelude::*;
 
-// fn main() {
-//     let data = vec![
-//         vec!["Vip level".to_string(), "Vip points".to_string()],
-//         vec!["0 > 1".to_string(), "100".to_string()],
-//         vec!["0 > 2".to_string(), "100\n200\n300".to_string()],
-//     ];
-
-//     println!("{}", ascii_table_generator::make_ascii_table(data));
-// }
-
 fn convert_table_data_from_array(table_data: Array) -> Vec<Vec<String>> {
     let mut rust_vec = Vec::new();
     for i in 0..table_data.length() {
